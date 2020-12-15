67196b852994b4f0a122ce093f50e19db2d8cc2e
zhaolc <zhaolc@cityfun.com.cn>
2020-12-15 18:16:03 +0800

    feature:人口学历分布及环形进度图组件抽吸

--- src/app/business/people-portrayal/poeple-dashboard/population-culture-ratio/population-culture-ratio.component.ts
+++ src/app/business/people-portrayal/poeple-dashboard/population-culture-ratio/population-culture-ratio.component.ts
@@ -8,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
 export class PopulationCultureRatioComponent implements OnInit {
 
   constructor() { }
-
+  tmpData=[
+    { type: '研究生', value: 0.2},
+    { type: '大学本科', value: 2},
+    { type: '大学专科', value: 3},
+    { type: '中专', value: 4},
+    { type: '高中', value: 8},
+    { type: '初中', value: 57},
+    { type: '小学', value: 17},
+    { type: '文盲', value: 1},
+  ]
   ngOnInit() {
   }
 
--- src/app/shared/components/circle-progress-graph/circle-progress-graph.component.ts
+++ src/app/shared/components/circle-progress-graph/circle-progress-graph.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-circle-progress-graph',
@@ -6,12 +6,45 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./circle-progress-graph.component.scss']
 })
 export class CircleProgressGraphComponent implements OnInit {
-
+  @Input() chartdata;
   constructor() { }
 
   ngOnInit() {
   }
   configureChart(chart){
+    let data=[this.chartdata]
+    data.push({ type: '其他', value: 100 - data[0].value });
+    chart.data(data);
+    chart.legend(false);
+    chart.coord("theta", {
+      radius: 1,
+      innerRadius: 0.8,
+    });
+    chart
+      .interval()
+      .position("value")
+      .adjust('stack')
+      .color('type',['#0a9afe',"rgba(116, 121, 120, 0.5)"])
+      .style({
+        opacity: 1,
+      });
+
+    chart.annotation().html({
+      offsetX:-12,
+      offsetY:15,
+      html: `
+              <div style="display:flex;
+                align-items:center;
+                justify-content:center;">
+                  <span style='
+                  font-family:DINAlternate-Bold,DINAlternate;
+                  font-size:40px
+                  font-weight:bold;
+                  color: #C5CCD3;;
+                  line-height:37px;'>${this.chartdata.value}%</span>
+              </div>`,
+    });
+    chart.render();
   }
 
 }
